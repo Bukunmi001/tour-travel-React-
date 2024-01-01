04b3557e6a486e5350f0626f4c69edfdcfbeb617
Bukunmi Ogundeji <ogundejibukunmi5@gmail.com>
2024-01-01 09:21:57 +0100

    first commit

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <>
+      <span id="location"></span>
+      <section data-aos="fade-up" className="">
+        <div className="container my-4">
+          <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
+            Location to visit
+          </h1>
+
+          <div className="rounded-xl ">
+            <iframe
+              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=105%20Oyemekun%20Road%20Akure,%20Ondo%20State+(Locations)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed "
+              width="100%"
+              height="360"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              style={{ borderRadius: "20px" }}
+            ></iframe>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Location;
---
+++ src/components/Places/Places.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import PlaceCard from "./PlaceCard";
+import Img1 from "../../assets/places/boat.jpg";
+import Img2 from "../../assets/places/tajmahal.jpg";
+import Img3 from "../../assets/places/water.jpg";
+import Img4 from "../../assets/places/place4.jpg";
+import Img5 from "../../assets/places/place5.jpg";
+import Img6 from "../../assets/places/place6.jpg";
+
+const PlacesData = [
+  {
+    img: Img1,
+    title: "Boat",
+    location: "USA",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 6700,
+    type: "Cultural Relax",
+  },
+  {
+    img: Img2,
+    title: "Lekki Conservation",
+    location: "Nigeria",
+    description:
+      "The Lekki Conservation Center  (LCC) as it is fondly called covers a land area of 78 hectares of land. This nature hub is run by the Nigerian conservation foundation (NCF) ",
+    price: 6700,
+    type: "Cultural Relax",
+  },
+  {
+    img: Img3,
+    title: "Underwater",
+    location: "US",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 6200,
+    type: "Cultural Relax",
+  },
+  {
+    img: Img4,
+    title: "Sydney",
+    location: "USA",
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 6700,
+    type: "Cultural Relax",
+  },
+  {
+    img: Img5,
+    title: "Los Angeles",
+    location: "United states",
+    description:
+"lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 6700,
+    type: "Cultural Relax",
+  },
+  {
+    img: Img6,
+    title: "Los Vegas",
+    location: "California",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: 6200,
+    type: "Cultural Relax",
+  },
+];
+
+const Places = ({ handleOrderPopup }) => {
+  return (
+    <>
+      <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10">
+        <section data-aos="fade-up" className="container ">
+          <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
+            Best Places to Visit
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {PlacesData.map((item, index) => (
+              <PlaceCard
+                handleOrderPopup={handleOrderPopup}
+                key={index}
+                {...item}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Places;
